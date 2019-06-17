0efec8064964600e459a24ba2efb1641eaea6811
bekverdyan <bekor@bk.ru>
2019-06-17 20:14:04 +0400

    added something like user service

--- src/app/static-data.ts
+++ src/app/static-data.ts
@@ -1,5 +1,40 @@
 import { User } from "./user";
 
-export class UserList {
-  user = new Array<User>();
+export class Users {
+  /**
+   * authenticate
+   */
+  public authenticate(username: string, password: string): User {
+    return this.user.filter((element, index, array) => {
+        return (element.name === username && element.password === password);
+    })[0];
+  }
+
+  /**
+   * find user by ID
+   */
+  public findById(id: string): User[] {
+    return this.user.filter((element, index, array) => {
+      return (element.id === id);
+    })
+  }
+
+  /**
+   * listUsers
+   */
+  public listUsers(): User[] {
+    return this.user;
+  }
+
+  private user = new Array<User>(
+    new User(),
+    new User('3646qelverc'),
+    new User('574707', 'nemo', 'asd'),
+    new User('34bg98', 'gago', 'asd'),
+    new User('d4r9j4', 'popok', 'asd'),
+    new User('0okm7y', 'krolik', 'asd'),
+    new User('e3dr8u', 'zubik', 'asd'),
+    new User('z2d04h', 'savok', 'asd'),
+    new User('fg7yr9', 'harthuk', 'asd')
+  );
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,4 +1,17 @@
-export interface User {
-  id: string;
-  name: string;
+export class User {
+  id = '';
+  name = '';
+  password = '';
+
+  constructor(id?: string, name?: string, password?: string) {
+    if (id !== undefined) {
+      this.id = id;
+    }
+    if (name !== undefined) {
+      this.name = name;
+    }
+    if (password !== undefined) {
+      this.password = password;
+    }
+  }
 }
